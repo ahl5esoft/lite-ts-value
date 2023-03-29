ecd61e19fe682d970d0d9b76ecc2de4c3df3fd43
连建伟 <3478289084@qq.com>
2023-03-29 09:42:35 +0800

    feat: 导出lucky-draw相关实现

--- src/lucky-draw-factory_test.ts
+++ src/lucky-draw-factory_test.ts
@@ -4,32 +4,29 @@ import { Mock, mockAny } from 'lite-ts-mock';
 import { RpcBase } from 'lite-ts-rpc';
 
 import { LuckyDrawFactory as Self } from './lucky-draw-factory';
+import { ValueService } from './value-service';
 
 describe('src/lucky-draw-factory.ts', () => {
     describe('.findLuckyDrawServices()', () => {
         it('ok', async () => {
             const enumFactoryMock = new Mock<EnumFactoryBase>();
             const rpcMock = new Mock<RpcBase>();
-            const self = new Self('lucky-draw', enumFactoryMock.actual, rpcMock.actual);
+            const valueServiceMock = new Mock<ValueService>();
+            const self = new Self('lucky-draw', enumFactoryMock.actual, rpcMock.actual, valueServiceMock.actual);
 
             rpcMock.expectReturn(
                 r => r.call(mockAny),
-                {
-                    userValues: {
-                        68: 0
-                    },
-                    luckyDraws: [
-                        {
-                            entry: {
-                                value: 1,
-                            },
-                            values: {}
-                        }
-                    ]
-                }
+                [
+                    {
+                        entry: {
+                            value: 1,
+                        },
+                        values: {}
+                    }
+                ]
             );
 
-            const res = await self.findLuckyDrawServices();
+            const res = await self.findLuckyDrawServices(null);
             notStrictEqual(res[1], undefined);
         });
     });
