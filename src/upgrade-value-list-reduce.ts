c457308a34dd625f730860aeabc1f8704deb4819
连建伟 <3478289084@qq.com>
2023-03-31 17:13:10 +0800

    feat: 代码优化,删除upgrade-value-list

--- src/upgrade-value-list-reduce.ts
+++
@@ -1,9 +0,0 @@
-import { UpgradeData } from './upgrade-data';
-import { UpgradeValueList } from './upgrade-value-list';
-
-export function upgradeValueReduce(memo: UpgradeValueList, r: UpgradeData) {
-    if (r.list)
-        memo[r.value] = r.list;
-
-    return memo;
-}
