e5adc6916f66b22728551db9a664da6558c3de9c
linhongjue <linhongjue@fengling.cn>
2023-04-06 09:38:56 +0800

    feat:过期数值代码调优

--- src/expire-time-handler-base.ts
+++ src/expire-time-handler-base.ts
@@ -4,9 +4,9 @@ import { ValueHandlerBase } from './value-handler-base';
 import { ValueHandlerOption } from './value-handler-option';
 import { Time, ValueTypeData } from './value-type-data';
 
-export abstract class ExpireTimeHanglerBase extends ValueHandlerBase {
+export abstract class ExpireTimeHandlerBase extends ValueHandlerBase {
 
-    constructor(
+    public constructor(
         protected getNowFunc: () => Promise<number>,
         private m_EnumFactory: EnumFactoryBase
     ) {
@@ -17,10 +17,10 @@ export abstract class ExpireTimeHanglerBase extends ValueHandlerBase {
         const allItem = await this.m_EnumFactory.build<ValueTypeData>(ValueTypeData.ctor, option.areaNo).allItem;
         const time = allItem[option.value.valueType]?.time;
         if (time?.expiredOnValueType)
-            await this.handleDiff(option, time);
+            await this.handling(option, time);
 
         await this.next?.handle?.(option);
     }
 
-    protected abstract handleDiff(option: ValueHandlerOption, time: Time): Promise<void>;
+    protected abstract handling(option: ValueHandlerOption, time: Time): Promise<void>;
 }
--- src/update-expire-time-handler.ts
+++ src/update-expire-time-handler.ts
@@ -1,16 +1,13 @@
-import { ExpireTimeHanglerBase } from './expire-time-handler-base';
+import { ExpireTimeHandlerBase } from './expire-time-handler-base';
 import { ValueHandlerOption } from './value-handler-option';
 import { Time } from './value-type-data';
 
-export class UpdateExpireTimeValueHandler extends ExpireTimeHanglerBase {
-    protected async handleDiff(option: ValueHandlerOption, time: Time) {
+export class UpdateExpireTimeValueHandler extends ExpireTimeHandlerBase {
+    protected async handling(option: ValueHandlerOption, time: Time) {
         if (!time.expireOn)
             return;
 
-        const now = await this.getNowFunc();
         const ownValue = await option.valueService.ownValue;
         ownValue[time.expiredOnValueType] = time.expireOn;
-        if (now > ownValue[time.expiredOnValueType])
-            ownValue[option.value.valueType] = 0;
     }
 }
